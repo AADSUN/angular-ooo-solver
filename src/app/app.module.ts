294539ec5092021521ae4e810aa623272aebf331
David Nguyen <deepfriedcoconut@gmail.com>
2018-07-08 16:41:47 +1000

    Added WikipediaService which obtains required information from Wikipedia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Form, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { CurrentinputsComponent } from './currentinputs/currentinputs.component';
 
 import { ItemService } from './services/item.service';
+import { WikipediaService } from './services/wikipedia.service';
 
 @NgModule({
   declarations: [
@@ -16,10 +18,12 @@ import { ItemService } from './services/item.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
-    ItemService
+    ItemService,
+    WikipediaService
   ],
   bootstrap: [AppComponent]
 })
